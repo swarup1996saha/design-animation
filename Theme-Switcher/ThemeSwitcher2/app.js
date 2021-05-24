4fdb88fa072d7b5e955ce523e8d6cdc2b506e0ba
Swarup Saha <65604993+swarup1996saha@users.noreply.github.com>
2021-05-24 19:29:27 +0530

    Delete Theme-Switcher directory

--- Theme-Switcher/ThemeSwitcher2/app.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:c8148abe1336cbb991d8a4cb33692ff669ee142c32f2e14b54a02d0fae713590
-size 426
--- Theme-Switcher/ThemeSwitcher4/app.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:af5b1191a983b54da40ca338ce754ed7b38cb395831b099e1ba758f8b806c0f8
-size 267
